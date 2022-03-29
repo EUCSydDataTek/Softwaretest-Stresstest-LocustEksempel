7dc958acb16f4cf0b6d9ee82c4d616a5fa93d5ac
jakob. spiel .køhler <jakobspielkohler@live.dk>
2022-03-29 08:48:38 +0200

    lavet template

--- locustfile.py
+++ locustfile.py
@@ -5,15 +5,3 @@ class TestClass(HttpUser):
     @task
     def go_to_homepage(self):
         self.client.get("/")
-
-    @task
-    def go_to_edit_page(self):
-        self.client.get("/Restaurants/Edit/2")
-
-    @task
-    def go_to_details_page(self):
-        self.client.get("/Restaurants/Detail/2")
-
-    @task
-    def go_to_privacy(self):
-        self.client.get("/Privacy")
